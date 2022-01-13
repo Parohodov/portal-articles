0d685786ee48d478a51201285cc33bdff22d683c
Pavel <82582511+Parohodov@users.noreply.github.com>
2022-01-13 19:15:09 +0300

    Initial state one more time

---
+++ src/main/java/ru/parohodov/portalarticles/PortalArticlesApplication.java
@@ -0,0 +1,13 @@
+package ru.parohodov.portalarticles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalArticlesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalArticlesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/parohodov/portalarticles/PortalArticlesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.parohodov.portalarticles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalArticlesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
